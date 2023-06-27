c0829aca10fde26181a6cebb5ae9bd593a3f59cd
Robert Rozic <rozic.r@gmail.com>
2023-06-27 15:18:11 +0200

    First commit

---
+++ src/utils/randomName.js
@@ -0,0 +1,7 @@
+export const randomName = () => {
+    const adjectives = ['Brzi', 'Ljuti', 'Gladan', 'Zgodan', 'Morski', 'Divlji']
+    const nouns = ['Mačak', 'Zec', 'Ris', 'Vuk', 'Galeb', 'Poskok', 'Tuljan']
+    const randomIndex = () => Math.floor(Math.random() * adjectives.length)
+  
+    return `${adjectives[randomIndex()]} ${nouns[randomIndex()]}`
+  }
